b7d3f47942a2f9f940624a7ae792035be5908a86
noeliapasaca <napasaca@espol.edu.ec>
2024-06-23 19:54:07 -0500

    Cambios

--- js/main.js
+++ js/main.js
@@ -1,11 +1,6 @@
 /*-------*/
 let loaded = (eventLoaded) => {
 
-  /* Primera parte alerta */
-  window.alert("landing page loaded");
-  console.log(eventLoaded);
-  debugger;
-
   /* Segunda parte - document API */
   let myform = document.getElementById('formu');
   debugger
@@ -16,12 +11,12 @@ let loaded = (eventLoaded) => {
     // Elementos
     var elemento1 = document.getElementById('nombre')
     var elemento2 = document.getElementById('email')
-    var elemento3 = document.getElementById('select')
+    
 
 
     var nombre = elemento1.value.trim();
     var email = elemento2.value.trim();
-    var select = elemento3.value.trim();
+    
 
     if (nombre.length == 0) {
       elemento1.focus();
@@ -33,11 +28,7 @@ let loaded = (eventLoaded) => {
       alert('Por favor, ingrese su correo electrónico.');
       return;
     }
-    if (select.length === 0) {
-      elemento3.focus();
-      alert('Por favor, ingrese la opcion deseada.');
-      return;
-    }
+    
 
     var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(email)) {
@@ -52,10 +43,10 @@ let loaded = (eventLoaded) => {
     const datos = {
       nombre: nombre,
       email: email,
-      select: select
+      
     };
 
-    fetch("https://prueba-a229e-default-rtdb.firebaseio.com/collection.json",
+    fetch("https://emprendimiento-66d26-default-rtdb.firebaseio.com/collection.json",
       {
         method: 'POST',
         body: JSON.stringify(datos),
@@ -74,79 +65,4 @@ let loaded = (eventLoaded) => {
 
 }
 
-window.addEventListener("DOMContentLoaded", loaded);
-
-async function obtenerDatos() {
-  const url = "https://prueba-a229e-default-rtdb.firebaseio.com/collection.json"; // Reemplaza con la URL real de la API o recurso
-  const respuesta = await fetch(url);
-  if (!respuesta.ok) {
-    console.error("Error:", respuesta.status);
-    return;
-  }
-  const datos = await respuesta.json();
-  let votesMap = new Map();
-
-  for (const key in datos) {
-    let vote = datos[key].select;
-    if (votesMap.has(vote)) {
-      votesMap.set(vote, votesMap.get(vote) + 1);
-    } else {
-      votesMap.set(vote, 1);
-    }
-  }
-
-
-  // Convertir el Map a un Array y ordenarlo en orden descendente por votos
-  let sortedVotes = Array.from(votesMap.entries()).sort((a, b) => b[1] - a[1]);
-  console.log(sortedVotes);
-  // Generar el HTML para cada elemento y agregarlo a la página
-  document.getElementById("get").innerHTML = '';
-  const places = ["Primer lugar", "Segundo lugar", "Tercer lugar"];
-  for (let i = 0; i < sortedVotes.length; i++) {
-    let [select, votes] = sortedVotes[i];
-    let template = `   
-        <div class="col-12 col-sm-6 col-md-4 mb-4 mb-md-0">
-					<div class="post-entry">
-						<img src="images/${select}.png" alt="${select}"
-								class="img-fluid">
-						<div class="post-content-entry">
-							<h3 >${places[i]}</h3>
-							<p><strong>Marca:</strong>: ${select}</p>
-							<p><strong>Votos</strong>: ${votes}</p>
-						</div>
-					</div>
-				</div>
-              
-      `;
-    document.getElementById("get").innerHTML += template;
-  }
-
-
-}
-
-obtenerDatos();
-
-async function rakingKits() {
-  const url = "https://lavender-59c67-default-rtdb.firebaseio.com/lavender.json";
-  const respuesta = await fetch(url);
-  if (!respuesta.ok) {
-    console.error("Error:", respuesta.status);
-    return;
-  }
-  const datos = await respuesta.json();
-  let votesMap = new Map();
-
-
-  for (const key in datos) {
-    let vote = datos[key].kit;
-    if (votesMap.has(vote)) {
-      votesMap.set(vote, votesMap.get(vote) + 1);
-    } else {
-      votesMap.set(vote, 1);
-    }
-  }
-
-
-}
-rakingKits();
 window.addEventListener("DOMContentLoaded", loaded);
